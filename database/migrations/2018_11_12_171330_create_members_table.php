9fe6239d5c0b5175b413a257667b26e6571b4412
Trịnh Hữu Vũ <vutrinhhuu@gmail.com>
2018-11-13 10:56:22 +0700

    Init database

---
+++ database/migrations/2018_11_12_171330_create_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('channel_name');
+            $table->integer('member_id')->unsigned();
+            $table->foreign('channel_name')->references('name')->on('channels');
+            $table->foreign('member_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
