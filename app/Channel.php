b9f5aeeb37b5ec2252279dc110380eed9fd6c3c4
DoVanDuyHedspi <vanduy07c.r@gmail.com>
2018-12-17 20:42:10 +0700

    get from url

--- app/Channel.php
+++ app/Channel.php
@@ -32,4 +32,21 @@ class Channel extends Model
     
         return $string.$di->i.':'.$di->s;
     }
+
+    function getYoutubeIdFromUrl($url) {
+        $parts = parse_url($url);
+        if(isset($parts['query'])){
+            parse_str($parts['query'], $qs);
+            if(isset($qs['v'])){
+                return $qs['v'];
+            }else if(isset($qs['vi'])){
+                return $qs['vi'];
+            }
+        }
+        if(isset($parts['path'])){
+            $path = explode('/', trim($parts['path'], '/'));
+            return $path[count($path)-1];
+        }
+        return false;
+    }
 }
